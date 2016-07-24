6f66ff045dbbc619f59f55045eb96e827a5a78ff
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-07-24 16:01:25 -0700

    add bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,4 @@
-def bubble_sort(array)
+def bubble_sort array
   i=0
   j=1
   while i < array.length
@@ -17,3 +17,28 @@ def bubble_sort(array)
   end
   array
 end
+
+def bubble_sort_by array
+  i = 0
+  j = 1
+  while i < array.length
+    if yield(array[i],array[j]) > 0
+      array[i],array[j] = array[j],array[i]
+    end
+    i+=1
+    j=i+1
+    if j > array.length
+      i = 0
+      j = 1
+      if array == array.sort
+        break
+      end
+    end
+  end
+  array
+end
+
+array = ["hi", "hello", "hey"]
+
+bubble_sort_by(array){|left, right| left.length - right.length}
+
